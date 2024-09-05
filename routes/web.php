b5e2b99419b11f1d9f92c5ca10a9bd8314d5f0d1
yogyy <yogimr261@gmail.com>
2024-09-05 23:58:10 +0700

    URL Generation

--- routes/web.php
+++ routes/web.php
@@ -92,6 +92,12 @@ Route::get('/redirect/to', [\App\Http\Controllers\RedirectController::class, 're
 Route::get('/redirect/name', [\App\Http\Controllers\RedirectController::class, 'redirectName']);
 Route::get('/redirect/name/{name}', [\App\Http\Controllers\RedirectController::class, 'redirectHello'])
     ->name('redirect-hello');
+Route::get('/redirect/named', function () {
+    //    return route('redirect-hello', ['name' => 'Klein']);
+    //    return url()->route('redirect-hello', ['name' => 'Klein']);
+    return \Illuminate\Support\Facades\URL::route('redirect-hello', ['name' => 'Klein']);
+});
+
 Route::get('/redirect/action', [\App\Http\Controllers\RedirectController::class, 'redirectAction']);
 Route::get('/redirect/away', [\App\Http\Controllers\RedirectController::class, 'redirectAway']);
 
@@ -100,11 +106,22 @@ Route::get('/redirect/away', [\App\Http\Controllers\RedirectController::class, '
 Route::middleware(['contoh:tarot-club,401'])->prefix('/middleware')->group(function () {
     Route::get('/api', function () {
         return 'OK';
-})->middleware('contoh:tarot-club,401');
+    });
+    Route::get('/group', function () {
+        return 'GROUP';
+    });
+});
 
-Route::get('/middleware/group', function () {
-    return 'GROUP';
-})->middleware('group-contoh');
+Route::get('/url/action', function () {
+    // return action([\App\Http\Controllers\FormController::class, 'form'], []);
+    // return url()->action([\App\Http\Controllers\FormController::class, 'form'], []);
+    return \Illuminate\Support\Facades\URL::action([\App\Http\Controllers\FormController::class, 'form'], []);
+});
 
 Route::get('/form', [\App\Http\Controllers\FormController::class, 'form']);
 Route::post('/form', [\App\Http\Controllers\FormController::class, 'submitForm']);
+
+
+Route::get('/url/current', function () {
+    return \Illuminate\Support\Facades\URL::full();
+});
